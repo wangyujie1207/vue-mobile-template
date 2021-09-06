8fe866bbaddd61eb83332367b3eccf062c0c8a16
wangyujie <82414199@163.com>
2021-09-06 23:52:45 +0800

    main import i18n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import i18n from '@/lang/register-i18n'
 import registerVant from '@/register_vant'
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
+app.use(i18n)
 app.use(registerVant)
 app.mount('#app')
